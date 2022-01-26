e6edfd0e8496e8e7bd0a93461cf656132222df48
Shahzad Naeem <shahzad65naeem@gmail.com>
2022-01-26 12:33:51 +0000

    Add Customer 1:1 Contact relation

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,24 +2,34 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function seed() {
+
+    const bob = await prisma.customer.create({
+        data: {
+            name: 'Bobby TABLES'
+        }
+    });
+
+    console.log('Customer `Bobby TABLES` created', bob);
+
     const createdCustomer = await prisma.customer.create({
         data: {
             name: 'Alice'
         }
     });
 
-    console.log('Customer created', createdCustomer);
+    console.log('Customer `Alice` created', createdCustomer);
 
     // Add your code here
 
     const createdContact = await prisma.contact.create({
         data: {
+            customerId: createdCustomer.id,
             phone: '0131 334 6523',
             email: 'alice@example.com'
         }
     });
 
-    console.log('Contact created', createdContact);
+    console.log('Contact `Alice` created - linked to Customer', createdContact);
 
     // Don't edit any of the code below this line
     process.exit(0);
