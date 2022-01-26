1cd81c48d827e20dc04354621f906313be5d64a6
Shahzad Naeem <shahzad65naeem@gmail.com>
2022-01-26 14:00:55 +0000

    Add Movie 1:Many Screening relation

--- prisma/seed.js
+++ prisma/seed.js
@@ -42,17 +42,27 @@ async function seed() {
 
     let screeningTime = new Date();
 
-    screeningTime.setHours( screeningTime.getHours() + 20 );
+    console.log( 'Time now: ', screeningTime );
+
+    // Set screening time to tomorrow at 8pm - there are libraries that will do this :)
+    const eightPM = 20;
+    const hrsToMillis = 60 * 60 * 1000;
+    const offset = ( 24 + ( eightPM - screeningTime.getHours() ) ) * hrsToMillis;
+
+    screeningTime.setTime( screeningTime.getTime() + offset );
     screeningTime.setMinutes( 0 );
     screeningTime.setSeconds( 0 );
 
+    console.log( 'Screening time: ', screeningTime );
+
     const aScreening = await prisma.screening.create({
         data: {
+            movieId:  dune.id,
             startsAt: screeningTime
         }
     });
 
-    console.log( 'Screening created', aScreening );
+    console.log( 'Screening for `Dune` created', aScreening );
 
     // Don't edit any of the code below this line
     process.exit(0);
