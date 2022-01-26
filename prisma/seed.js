f2fc9527ebff17eae18df99a9bcf2c414bf04ca4
Shahzad Naeem <shahzad65naeem@gmail.com>
2022-01-26 12:54:26 +0000

    Add Movie and Screening

--- prisma/seed.js
+++ prisma/seed.js
@@ -31,6 +31,29 @@ async function seed() {
 
     console.log('Contact `Alice` created - linked to Customer', createdContact);
 
+    const dune = await prisma.movie.create({
+        data: {
+            title: "Dune",
+            runTimeMins: 170
+        }
+    });
+
+    console.log('Movie `Dune` created', dune );
+
+    let screeningTime = new Date();
+
+    screeningTime.setHours( screeningTime.getHours() + 20 );
+    screeningTime.setMinutes( 0 );
+    screeningTime.setSeconds( 0 );
+
+    const aScreening = await prisma.screening.create({
+        data: {
+            startsAt: screeningTime
+        }
+    });
+
+    console.log( 'Screening created', aScreening );
+
     // Don't edit any of the code below this line
     process.exit(0);
 }
